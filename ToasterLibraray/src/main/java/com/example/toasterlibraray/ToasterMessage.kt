4e0c6e3db341962e2793af1ef56849304697deea
wasim abbas <wasim.abbas1603@gmail.com>
2021-12-09 22:00:03 +0500

    first comit

--- ToasterLibraray/src/main/java/com/example/toasterlibraray/ToasterMessage.kt
+++ ToasterLibraray/src/main/java/com/example/toasterlibraray/ToasterMessage.kt
@@ -3,7 +3,7 @@ package com.example.toasterlibraray
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
+object ToasterMessage {
     fun toastMessage(context:Context,message:String)
     {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
